28fc60ac173605a25b69e7d902d429eb853ecb91
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-05-30 20:30:29 +1000

    complete exercises and create files

---
+++ Session_1/exercises.py
@@ -0,0 +1,15 @@
+# Exercise 1
+first_number = input ("Enter a number")
+second_number = input ("Enter another number")
+print(f"The total of your numbers is {int(first_number)+int(second_number)}") 
+
+# Exercise 2
+first_number = input ("Enter a number")
+second_number = input ("Enter another number")
+print(f"Your numbers multiplied are {int(first_number)*int(second_number)}") 
+
+# Exercise 3
+distance_km = input ("How far are you going in kms?")
+print(f"You have {int(distance_km)*100} meters to go and {int(distance_km)*1000} centimeters to go")
+
+#Exercise 5: Input function is an expression
---
+++ Session_1/variables.py
@@ -0,0 +1,36 @@
+# Create a variable names first_name and assign Amanda to it
+first_name = "Amanda"
+last_name = "Hagaman"
+programming_language = "Python"
+
+# Data Types - Text data, numerical data
+# Text data - String
+today = "Tuesday"
+# print(today)
+# print(f"Today is {today}")
+# print(type(today))
+
+# Numerical data types - Integer, Float
+# Integer - whole number, no need to use quotes
+parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+my_height = 168
+# print(f"Amanda is {my_height} centimetres tall")
+
+# Float
+my_weight = 60
+# print(type(my_weight))
+# print(f"My weight is {my_weight} in kg")
+
+distance = "5000"
+# print(int(distance) + 8)
+# print(distance + str(8))
+
+# print("hello")
+# dog_name = input("What's the dog's name?")
+# print(f"Nice to meet you {dog_name}")
+
+# Input function always stores data as a string
+birth_year = input("What year were you born?")
+print(f"You are {2023-int(birth_year)} years old")
