626339e1acb3e969b4e84b7cf71c01ef62125e5d
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-06-03 09:49:53 +1000

    Create list of expressions

---
+++ Session_2/conditionals.py
@@ -0,0 +1,28 @@
+#Booleen
+name = "Amanda"
+age = 36
+height = 1.5
+is_raining = True
+my_variable2 = False
+print(is_raining)
+print(type(is_raining))
+
+#Booleen Expressions - Expressions that produce a Booleen value
+# Comparison Operators
+# == equal to
+# != is not equal to
+# > greater than
+# < less than
+# >= great or equal to
+# <= less than or equal to
+# print (5 > 3)
+# print (3.5 > 6.3)
+# print ("Amanda" == "amanda")
+
+#Mathematical Operators - Addition, division, subtraction
+#Boolean Operators - not, and, or
+is_sunny = True
+is_warm = True
+#print(not_is_sunny)
+print(is_sunny and is_warm)
+print(is_sunny or is_warm)
