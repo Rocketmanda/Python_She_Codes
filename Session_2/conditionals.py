589276ae5eccf08e540016c2e7e8415dbae4fd74
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-06-03 10:18:41 +1000

    add examples to conditionals

--- Session_2/conditionals.py
+++ Session_2/conditionals.py
@@ -1,13 +1,13 @@
-#Booleen
+#Boolean
 name = "Amanda"
 age = 36
 height = 1.5
 is_raining = True
 my_variable2 = False
-print(is_raining)
-print(type(is_raining))
+# print(is_raining)
+# print(type(is_raining))
 
-#Booleen Expressions - Expressions that produce a Booleen value
+#Boolean Expressions - Expressions that produce a Booleen value
 # Comparison Operators
 # == equal to
 # != is not equal to
@@ -24,5 +24,18 @@ print(type(is_raining))
 is_sunny = True
 is_warm = True
 #print(not_is_sunny)
-print(is_sunny and is_warm)
-print(is_sunny or is_warm)
+# print(is_sunny and is_warm)
+# print(is_sunny or is_warm)
+
+temperature = 20
+# syntax / semantics
+if temperature < 18:
+    print("Weather is too cold!")
+elif temperature > 28: #if statement is false, it will then check elif (else if)
+    print("Weather is too hot")
+else:
+    print("Weather is nice!")
+
+#multiple if statements - will check the expression 
+# print("My dog looks cute")
+
