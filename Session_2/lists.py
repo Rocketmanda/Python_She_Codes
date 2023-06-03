0e066e5d03978434a0bfb5c15f1f4b5c227d2c20
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-06-03 19:36:59 +1000

    Add lists

--- Session_2/lists.py
+++ Session_2/lists.py
@@ -1,16 +1,16 @@
 #Store multiple data points using lists
 #Lists can contain different data types
 #Lists are index-based starting from 0
-digits = [1,2,3,4,5]
-print(digits)
-digits.append(6)
-print(digits)
-digits.pop(0)
-print(digits)
-popped_element = digits.pop(0)
-#print(popped_element)
-digits [1] = 90
-print(digits)
+# digits = [1,2,3,4,5]
+# print(digits)
+# digits.append(6)
+# print(digits)
+# digits.pop(0)
+# print(digits)
+# popped_element = digits.pop(0)
+# #print(popped_element)
+# digits [1] = 90
+# print(digits)
 
 # print(list_name)
 # print(type(list_name))
@@ -22,4 +22,7 @@ print(digits)
 # print(digits[0:4]) #Start (inclusive) and End index (excluded)
 # print(digits [0:5:2])
 
-print(len(digits)) #Length of list by index total
+# print(len(digits)) #Length of list by index total
+
+letters = ['a','b','c','d',['Emily','Julie']] #Nested list
+print(letters[4][0]) #Selecting the element then 
