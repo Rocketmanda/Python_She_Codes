0f126ca561b060516987c006dd2d4bdc24b9b30e
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-06-03 12:19:38 +1000

    add code

---
+++ Session_2/exercises.py
@@ -0,0 +1,43 @@
+sara_has_helmet = False
+rei_has_rope = False
+
+# Q1
+#if sara_has_helmet == True:
+    # print("Let's send it!")
+# else sara_has_helmet == False:
+#     print("No way, my brain is my moneymaker!")
+
+# Q2
+# if sara_has_helmet == True and rei_has_rope == True:
+#     print("Let's send it!")
+# if sara_has_helmet == False and rei_has_rope == True:
+#     print("No way, my brain is my moneymaker")
+# if sara_has_helmet == True and rei_has_rope == False:
+#     print("Who's unprepared now, Rei?")
+# if sara_has_helmet == False and rei_has_rope == False:
+#     print("I guess let's just go hiking")
+
+# Q3
+# light_colour = "Red"
+# car_detected = True
+
+
+# if light_colour == "Red" and car_detected == True:
+#     print("Flash!")
+# else:
+#     print("Do Nothing")
+
+# Q4
+# user_height = int(input("How tall are you?"))
+# if user_height >= 120:
+#     print("Hop on!")
+# else:
+#     print("Sorry, not today")
+
+# Q5
+username = input("enter your username?")
+password = input ("enter your password")
+if username == "lucyg" and password == "quartzgleam?1":
+    print("Logged in successfully")
+else:
+    print("Access denied")
---
+++ Session_2/lists.py
@@ -0,0 +1,25 @@
+#Store multiple data points using lists
+#Lists can contain different data types
+#Lists are index-based starting from 0
+digits = [1,2,3,4,5]
+print(digits)
+digits.append(6)
+print(digits)
+digits.pop(0)
+print(digits)
+popped_element = digits.pop(0)
+#print(popped_element)
+digits [1] = 90
+print(digits)
+
+# print(list_name)
+# print(type(list_name))
+
+# print(digits[4]) #IndexError: there's an index element out of range, fix it
+# print(digits [-1]) #gives the very last index item ie. -1 last, -2 second last etc.
+
+#Slicing a list
+# print(digits[0:4]) #Start (inclusive) and End index (excluded)
+# print(digits [0:5:2])
+
+print(len(digits)) #Length of list by index total
