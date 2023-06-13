f2226e82c1bcce816c3a7107bfc23e0fb84e19f4
Amanda Hagaman <120612255+Rocketmanda@users.noreply.github.com>
2023-06-13 19:47:30 +1000

    Create Session

--- Session_4/functions.py
+++ Session_4/functions.py
@@ -1,32 +1,41 @@
-# Function - an activity that is natural tor or the purpose of a person or thing
-# input(), len(), int(), print() 
-#Every function has brackets syntax
+# # Function - an activity that is natural tor or the purpose of a person or thing
+# # input(), len(), int(), print() 
+# #Every function has brackets syntax
 
-#Function definition
+# #Function definition
 
-name = input("What is your name?")
-age = input("How old are you?")
-# if age >=18:
-#     print("Welcome")
-# else:
-#     print("You can not enter")
+# name = input("What is your name?")
+# age = input("How old are you?")
+# # if age >=18:
+# #     print("Welcome")
+# # else:
+# #     print("You can not enter")
 
-#Task Separation
-def ask user_name():
-    # print("Now function is entered")
-    name = input("What is your name?")
-    return name
-    print("hello")
+# #Task Separation
+# def ask user_name():
+#     # print("Now function is entered")
+#     name = input("What is your name?")
+#     return name
+#     print("hello")
 
-# print("Hello")
-answer = ask_user_name()
-print("answer")
-# print("Hi")
+# # print("Hello")
+# answer = ask_user_name()
+# print("answer")
+# # print("Hi")
     
 
-# def ask user_age():
-#     name = input("How old are you?")
-#     if age >= 18:
-#         print("Welcome")
-#     else:
-#         print("You can not enter")
+# # def ask user_age():
+# #     name = input("How old are you?")
+# #     if age >= 18:
+# #         print("Welcome")
+# #     else:
+# #         print("You can not enter")
+
+# Parameters
+def add(number1, number2):
+    result = number1 + number2
+    return(result)
+
+answer = add(2,3)
+print(answer)
+print(result) # Local Variable
